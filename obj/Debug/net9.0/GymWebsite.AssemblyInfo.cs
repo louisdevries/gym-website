4cee9ed7daea4ad72e8a422ae01a0b53984930b8
louisdevries <louisdevries89@gmail.com>
2025-02-12 14:59:06 +0200

    home page

--- obj/Debug/net9.0/GymWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/GymWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c302fb1f32635abaa7dbd2d70a8daf76bd4d5360")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
